f538a8b8f20851dccddf35c1cb4b2a945fccb34f
Aditya Malladi <aditya@keyri.co>
2022-07-14 15:31:47 -0400

    improve test app

--- Example/keyri-pod/ViewController.swift
+++ Example/keyri-pod/ViewController.swift
@@ -11,6 +11,11 @@ import keyri_pod
 import Toaster
 
 class ViewController: UIViewController {
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var message: UITextField!
+    
+    
     @IBAction func auth(_ sender: Any) {
         print("calling scanner")
         let scanner = keyri_pod.Scanner()
@@ -22,6 +27,12 @@ class ViewController: UIViewController {
         scanner.show(from: self)
     }
     
+    override func viewDidLoad() {
+        self.statusLabel.isHidden = true
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard (_:)))
+        self.view.addGestureRecognizer(tapGesture)
+    }
+    
     func process(url: URL) {
         let sessionId = URLComponents(url: url, resolvingAgainstBaseURL: true)?.queryItems?.first(where: { $0.name == "sessionId" })?.value ?? ""
 
@@ -31,26 +42,44 @@ class ViewController: UIViewController {
         
 
         let keyri = Keyri() // Be sure to import the SDK at the top of the file
-        keyri.initializeQrSession(username: "testuser@example.com", sessionId: sessionId, appKey: appKey) { res in
-            switch res {
-            case .success(var session):
-                // You can optionally create a custom screen and pass the session ID there. We recommend this approach for large enterprises
-                session.payload = "TestPayload"
+        keyri.initializeQrSession(username: username.text ?? "ANON", sessionId: sessionId, appKey: appKey) { res in
+            DispatchQueue.main.async {
+                switch res {
+                case .success(var session):
+                    // You can optionally create a custom screen and pass the session ID there. We recommend this approach for large enterprises
+                    session.payload = "\(self.username.text ?? "ANON") says \(self.message.text ?? "nothing")"
 
-                // In a real world example you’d wait for user confirmation first
-                do {
-                    print("confirming session")
-                    try session.confirm() // or session.deny()
-                } catch {
+                    // In a real world example you’d wait for user confirmation first
+                    do {
+                        print("confirming session")
+                        try session.confirm() // or session.deny()
+                        
+                            self.showAlert(title: "Success", message: "\(self.username.text) logged in" )
+                        
+                    } catch {
+                        print(error)
+                        self.showAlert(title: "Error", message: error.localizedDescription)
+                    }
+                case .failure(let error):
                     print(error)
+                    self.showAlert(title: "Error", message: error.localizedDescription)
                 }
-            case .failure(let error):
-                print(error)
             }
-            
         }
 
         
+        
+    }
+    
+    func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        self.present(alert, animated: true, completion: nil)
+        Timer.scheduledTimer(withTimeInterval: 3.0, repeats: false, block: { _ in alert.dismiss(animated: true, completion: nil)} )
+    }
+    
+    @objc func dismissKeyboard (_ sender: UITapGestureRecognizer) {
+        username.resignFirstResponder()
+        message.resignFirstResponder()
         
     }
     
