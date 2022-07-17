7e25a3bb4be91311c7d7874330e07e1a411d418d
Aditya Malladi <aditya@keyri.co>
2022-07-17 09:26:48 -0400

    test app improvements

--- Example/keyri-pod/ViewController.swift
+++ Example/keyri-pod/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var statusLabel: UILabel!
     @IBOutlet weak var username: UITextField!
     @IBOutlet weak var message: UITextField!
+    @IBOutlet weak var appKeySelector: UIPickerView!
+    
+    var pickerData = [["IT7VrTQ0r4InzsvCNJpRCRpi1qzfgpaj", "GZuwAB3QQQ5eTD4st5KmDPDD9PO0xak2", "ZTj8bC8XUUDNEtNw0FBLR6KYbhi7JO9S", "1F3FAZaZ9pcH9DNxyUGoTfi5IF9iqhh6"]]
+    var selectedAppKey = "IT7VrTQ0r4InzsvCNJpRCRpi1qzfgpaj"
     
     
     @IBAction func auth(_ sender: Any) {
@@ -31,13 +35,17 @@ class ViewController: UIViewController {
         self.statusLabel.isHidden = true
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard (_:)))
         self.view.addGestureRecognizer(tapGesture)
+        
+        self.appKeySelector.delegate = self
+        self.appKeySelector.dataSource = self
+        
     }
     
     func process(url: URL) {
         let sessionId = URLComponents(url: url, resolvingAgainstBaseURL: true)?.queryItems?.first(where: { $0.name == "sessionId" })?.value ?? ""
 
 
-        let appKey = "IT7VrTQ0r4InzsvCNJpRCRpi1qzfgpaj" // Get this value from the Keyri Developer Portal
+        let appKey = selectedAppKey // Get this value from the Keyri Developer Portal
         print("\n In callback \n")
         
 
@@ -82,5 +90,30 @@ class ViewController: UIViewController {
         message.resignFirstResponder()
         
     }
+    
+}
+
+extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        pickerData[component].count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[component][row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        self.selectedAppKey = pickerData[component][row]
+        print("CHANGING APPKEY \n")
+        print(self.selectedAppKey)
+    }
+    
+    
+    
+    
     
 }
