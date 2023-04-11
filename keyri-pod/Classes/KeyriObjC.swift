0c357ae64f49a69b65e2b1e33a09fc60876d17de
Aditya Malladi <aditya@keyri.co>
2023-04-11 11:07:01 -0400

    bridging

--- keyri-pod/Classes/KeyriObjC.swift
+++ keyri-pod/Classes/KeyriObjC.swift
@@ -8,13 +8,22 @@
 import Foundation
 
 public class KeyriObjC: NSObject {
-    let keyri: Keyri
+    var keyri: Keyri?
     
     @objc public override init() {
-        keyri = Keyri(appKey: "")
+        keyri = nil
+    }
+    
+    @objc public func initializeKeyri(appKey: String, publicAPIKey: String?) {
+        keyri = Keyri(appKey: appKey, publicApiKey: publicAPIKey)
     }
     
     @objc public func easyKeyriAuth(publicUserId: String, payload: String, completion: @escaping ((Bool, Error?) -> ())) {
+        guard let keyri = keyri else {
+            completion(false, KeyriErrors.notInitializedError)
+            return
+        }
+        
         keyri.easyKeyriAuth(publicUserId: publicUserId, payload: payload) { result in
             switch result {
             case .success(let bool):
@@ -26,6 +35,11 @@ public class KeyriObjC: NSObject {
     }
     
     @objc public func processLink(url: URL, publicUserId: String, appKey: String, payload: String, completion: @escaping ((Bool, Error?) -> ())) {
+        guard let keyri = keyri else {
+            completion(false, KeyriErrors.notInitializedError)
+            return
+        }
+        
         keyri.processLink(url: url, publicUserId: publicUserId, appKey: appKey, payload: payload) { result in
             switch result {
             case .success(let bool):
@@ -37,6 +51,11 @@ public class KeyriObjC: NSObject {
     }
     
     @objc public func initiateQrSession(username: String?, sessionId: String, appKey: String, completion: @escaping ((Session?, Error?) -> ())) {
+        guard let keyri = keyri else {
+            completion(nil, KeyriErrors.notInitializedError)
+            return
+        }
+        
         keyri.initiateQrSession(username: username, sessionId: sessionId) { result in
             switch result {
             case .success(let session):
@@ -48,10 +67,19 @@ public class KeyriObjC: NSObject {
     }
     
     @objc public func initializeDefaultConfirmationScreen(session: Session, payload: String,  completion: @escaping (Bool) -> ()) {
+        guard let keyri = keyri else {
+            completion(false)
+            return
+        }
+        
         keyri.initializeDefaultConfirmationScreen(session: session, payload: payload, completion: completion)
     }
     
     @objc public func generateAssociationKey(username: String?) throws -> String {
+        guard let keyri = keyri else {
+            throw KeyriErrors.notInitializedError
+        }
+        
         if let username = username {
             return try keyri.generateAssociationKey(username: username).derRepresentation.base64EncodedString()
         } else {
@@ -60,6 +88,10 @@ public class KeyriObjC: NSObject {
     }
     
     @objc public func generateUserSignature(username: String?, data: Data) throws -> String {
+        guard let keyri = keyri else {
+            throw KeyriErrors.notInitializedError
+        }
+        
         if let username = username {
             return try keyri.generateUserSignature(for: username, data: data).derRepresentation.base64EncodedString()
         } else {
@@ -68,6 +100,10 @@ public class KeyriObjC: NSObject {
     }
     
     @objc public func getAssociationKey(username: String?) throws -> String {
+        guard let keyri = keyri else {
+            throw KeyriErrors.notInitializedError
+        }
+        
         if let username = username, let associationKey = try keyri.getAssociationKey(username: username) {
             return associationKey.derRepresentation.base64EncodedString()
         } else if let associationKey = try keyri.getAssociationKey() {
@@ -78,6 +114,10 @@ public class KeyriObjC: NSObject {
     }
     
     @objc public func removeAssociationKey(publicUserId: String?) throws {
+        guard let keyri = keyri else {
+            throw KeyriErrors.notInitializedError
+        }
+        
         if let publicUserId = publicUserId {
             try keyri.removeAssociationKey(publicUserId: publicUserId)
         } else {
@@ -86,10 +126,27 @@ public class KeyriObjC: NSObject {
     }
     
     @objc public func listAssociactionKeys() -> [String:String]? {
+        guard let keyri = keyri else { return nil }
         return keyri.listAssociactionKeys()
     }
     
     @objc public func listUniqueAccounts() -> [String:String]? {
+        guard let keyri = keyri else { return nil }
         return keyri.listUniqueAccounts()
     }
+    
+    public func sendEvent(username: String = "ANON", eventType: EventType = .visits, success: Bool = true, completion: @escaping (FingerprintResponse?, Error?) -> ()) throws {
+        guard let keyri = keyri else { throw KeyriErrors.notInitializedError }
+        return try keyri.sendEvent(username: username, eventType: eventType, success: success) { res in
+            switch res {
+            case .success(let response):
+                completion(response, nil)
+            case .failure(let error):
+                completion(nil, error)
+            }
+        }
+    }
+    
+    
 }
+
